477357c984a4789768ab00a4d6d53e747b9f83af
skywalker <houzhenhong@outlook.com>
2020-06-23 17:53:10 +0800

    feat: 完成环境搭建并显示到 VGA 中

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // 不使用标准库，因为标准库中有许多与系统相关的东西
+#![no_main] // 禁止所有 rust 入口点
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World";
+
+/// 这个方法会 panic 的时候会调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+#[no_mangle] // no_mangle 不会对 _start 进行修改
+pub extern "C" fn _start() -> ! {  // 此函数是入口点，因为链接器会查找函数
+    // 0xb8000 是 VGA 的固定规定的地址
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // 浅青绿
+        }
+    }
+
+    loop {}
 }
