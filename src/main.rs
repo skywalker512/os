b2e5d55280287563b17b4068c6f5acb1cbd397ba
skywalker <houzhenhong@outlook.com>
2020-06-23 21:26:36 +0800

    feat: 测试结果通过串口传输到宿主机

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 #![reexport_test_harness_main = "test_main"] // 指定测试的入口
 
 use core::panic::PanicInfo;
+mod serial;
 
 mod vga_buffer;
 
@@ -27,15 +28,33 @@ pub extern "C" fn _start() -> ! {  // 此函数是入口点，因为链接器会
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
+    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
 
 #[cfg(test)] // 条件编译只有测试的时候才会编译
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        // 0xf4 是 package.metadata.bootimage 中指定的
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
     }
 }
